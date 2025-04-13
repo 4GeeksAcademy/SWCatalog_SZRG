aadbd1e01f0f6e82b6ea04d570445275917248b0
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-04-13 21:58:34 +0200

    FirstChanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-dark bg-dark">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+				
+				<span className="navbar-brand mb-0 h1">Star Wars Catalog</span>
+				
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Favorites</button>
 					</Link>
 				</div>
 			</div>
